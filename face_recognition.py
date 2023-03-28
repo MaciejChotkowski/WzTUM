3f3bc093d4f9fbca68c51abd0cd77eda028db3ad
Maciej Chotkowski <ma.chotkowski@gmail.com>
2023-03-28 16:03:53 +0200

    added basic opencv face recognition

---
+++ face_recognition.py
@@ -0,0 +1,23 @@
+import cv2
+import sys
+
+path_to_cascade_classifier = sys.argv[1]
+face_cascade = cv2.CascadeClassifier(path_to_cascade_classifier)
+
+# 0 is the default webcam's index
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
