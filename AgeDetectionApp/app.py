0af0b619383e4ac60e3c65b8226522ccc8ca1bdd
Maciej Chotkowski <ma.chotkowski@gmail.com>
2023-05-23 12:04:32 +0200

    mostly done, kinda slow

--- AgeDetectionApp/app.py
+++ AgeDetectionApp/app.py
@@ -1,8 +1,12 @@
+import tensorflow as tf
+import os
+import cv2
 from enum import Enum
 # from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 import imghdr
+from tensorflow import keras
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -15,12 +19,14 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
+from PIL import Image
 
-import cv2
-import os
+# Load model
+modelFile = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
+configFile = "deploy.prototxt"
+net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
 
-import tensorflow as tf
-import os
+
 print("Num of GPUs available: ", len(tf.test.gpu_device_name()))
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -39,9 +45,37 @@ capture = cv2.VideoCapture(0)
 agemodel = None
 
 
-def process_and_predict(im):
-    global agemodel
-    # im = Image.open(file).convert('L')
+def detect_faces(frame):
+    blob = cv2.dnn.blobFromImage(cv2.resize(
+        frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    net.setInput(blob)
+    detections = net.forward()
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array(
+                [frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
+            (x, y, w, h) = box.astype("int")
+
+            x = max(0, x - 20)  # subtract from the x-coordinate (left bound)
+            y = max(0, y - 20)  # subtract from the y-coordinate (top bound)
+            # add to the w-coordinate (right bound)
+            w = min(frame.shape[1], w + 20)
+            # add to the h-coordinate (bottom bound)
+            h = min(frame.shape[0], h + 20)
+
+            cv2.rectangle(frame, (x, y), (w, h), (0, 255, 0), 2)
+            extracted_face = frame[y:h, x:w]
+            cv2.imshow('face', extracted_face)
+            cv2.imwrite('extracted_face.png', extracted_face)
+
+            age_str = process_and_predict("extracted_face.png")
+            # print(age_str)
+            return age_str
+
+
+def process_and_predict(file):
+    im = Image.open(file).convert('L')
     width, height = im.size
     if width == height:
         im = im.resize((200, 200), Image.ANTIALIAS)
@@ -64,27 +98,26 @@ def process_and_predict(im):
     ar = np.asarray(im)
     ar = ar.astype('float32')
     ar /= 255.0
+    # caly resizing moze tez mozna prosciej w cv2
     ar = ar.reshape(1, 200, 200, 1)
     age = agemodel.predict(ar)
 
     age = np.argmax(age)
 
-    if age == 0:
-        print('Age: [1, 2]')
+    if age == 0:  # to trzeba zmienic, jak beda inne przedzialy, albo liczba po prostu
+        return ('Age: [1, 2]')
     elif age == 1:
-        print('Age: [3, 9]')
+        return ('Age: [3, 9]')
     elif age == 2:
-        print('Age: [10, 20]')
+        return ('Age: [10, 20]')
     elif age == 3:
-        print('Age: [21, 27]')
+        return ('Age: [21, 27]')
     elif age == 4:
-        print('Age: [28, 45]')
+        return ('Age: [28, 45]')
     elif age == 5:
-        print('Age: [46, 65]')
+        return ('Age: [46, 65]')
     else:
-        print('Age: (65, 80]')
-
-    return im.resize((300, 300), Image.ANTIALIAS)
+        return ('Age: (65, 80]')
 
 
 class LoadDialog(FloatLayout):
@@ -94,6 +127,7 @@ class LoadDialog(FloatLayout):
 
 class Root(Widget):
     image = ObjectProperty(None)
+    age_label = ObjectProperty(None)
 
     def switch_to_camera(self):
         global input_type, capture
@@ -129,8 +163,10 @@ class Root(Widget):
         self._popup.dismiss()
 
     def load_model(self, filename):
+        global agemodel
         if len(filename) > 0:
             load_file_path = os.path.join(filename[0])
+            agemodel = keras.models.load_model(load_file_path)
         self.dismiss_popup()
 
     def load_video(self, filename):
@@ -184,7 +220,9 @@ class CamApp(App):
             size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
         texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         self.layout.image.texture = texture1
-        # process_and_predict(texture1)
+        if agemodel is not None:
+            age_string = detect_faces(frame)
+            self.layout.age_label.text = age_string
 
     def display_video(self):
         global capture
